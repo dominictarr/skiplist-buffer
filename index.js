3d583bb91a687afc647c59f2137b1564d5b9403a
Dominic Tarr <dominic.tarr@gmail.com>
2018-06-29 11:26:03 -1000

    implement async

--- index.js
+++ index.js
@@ -34,6 +34,38 @@ function find(b, ptr, target, level) {
   return ptr
 }
 
+function findAsync(b, get, ptr, target, level, _cb) {
+  level = level || r_levels(b, ptr)
+
+  function cb (err, ptr) {
+    get(r_value(b, ptr), function (err, value) {
+      _cb(err, value, ptr)
+    })
+  }
+
+  ;(function next (ptr, _value) {
+    if(level < 0) return cb(null, _value, ptr)
+    //if this level is last item
+      var next_ptr = r_level(b, ptr, level)
+      if(next_ptr === 0) {
+        level --
+        if(level < 0)
+          cb(null, ptr)
+        else next(ptr)
+      }
+      else
+        get(r_value(b, next_ptr), function (err, value) {
+          if(compare(value, target) > 0)
+            level --
+          else
+            ptr = next_ptr
+
+          if(level < 0) cb(null, ptr)
+          else next(ptr)
+        })
+
+  })(ptr)
+}
 
 function insert (b, ptr, target, level) {
   level = level || r_levels(b, ptr)
@@ -77,9 +109,74 @@ function insert (b, ptr, target, level) {
   return free
 }
 
+function insertAsync (b, get, ptr, target, offset, level, cb) {
+  level = level || r_levels(b, ptr)
+  var free = b.readUInt32LE(0) || 8
+  //figure out before hand how many levels deep we want to insert this value
+  for(var _level = 0; _level < level && Math.random() > 0.5; )
+    _level ++
+
+  b.writeUInt32LE(free+4+4+_level*4, 0) //make space for this item.
+
+  //the value we want to insert
+  b.writeUInt32LE(offset, free)
+
+  ;(function next (ptr) {
+    console.log('next:', ptr)
+    if(level < 0) return cb(null, ptr)
+    //if this level is last item
+    var next_ptr = r_level(b, ptr, level)
+    if(next_ptr === 0) {
+      level --
+      if(level < 0) write()
+      else next(ptr)
+    }
+    else
+      get(r_value(b, next_ptr), function (err, value) {
+        if(err) return cb(err)
+        console.log('cmp', value, target, level)
+        if(compare(value, target) > 0) {
+          if(level <= _level)
+            b.writeUInt32LE(ptr, free+4+(4*level))
+          level --
+        }
+        else
+          ptr = next_ptr
+
+        if(level < 0) write()
+        else next(ptr)
+    })
+  })(ptr)
+
+  function write () {
+    //I think if insert items bottom up, and set the new pointer first
+    //then this should be fully threadsafe.
+    for(var i = 0; i <= _level; i++) {
+      var prev = b.readUInt32LE(free+4+i*4)
+      console.log('next:', prev)
+      var next = b.readUInt32LE(prev+4+i*4) //includes msb continue flag
+      b.writeInt32LE(i < _level ? next | 0x80000000 : next & 0x7fffffff, free+4+i*4)
+      //free, free | (next & 0x80000000))
+      b.writeInt32LE(free | (next & 0x80000000), prev+4+i*4)
+    }
+    cb(null, free)
+  }
+}
+
+
 module.exports = {
-  find: find,
-  insert: insert, get: r_value,
+  find: find, insert: insert,
+  findAsync: findAsync, insertAsync: insertAsync,
+  get: r_value,
   next: r_level, levels: r_levels
 }
+
+
+
+
+
+
+
+
+
 
