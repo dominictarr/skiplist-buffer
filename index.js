1db11e67e0d6e952dfd3aa544d5e01d5bda4d4a0
Dominic Tarr <dominic.tarr@gmail.com>
2018-06-28 22:01:01 -1000

    tidy

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
+'use strict'
 function r_value (b, ptr) {
   return b.readUInt32LE(ptr)
 }
 
 function r_level (b, ptr, level) {
   return b.readUInt32LE(ptr+4+level*4) & 0x7fffffff
-}
-
-function r_has_more (b, ptr, level) {
-  return b.readInt32LE(ptr) >= 0
-}
-
-function item (b, value, levels) {
-  if(levels.length < 1) throw new Error('levels must have at least [0]')
-  var free = r_value(b, 0) //read pointer to the very first item.
-  if(free == 0) {
-    free = 8
-    b.writeInt32LE(8, 4) //we are adding the first item, so set pointer.
-  }
-  b.writeUInt32LE(value, free)
-  for(var i = 0; i < levels.length; i++)
-    b.writeInt32LE(
-      levels[i] | ((i + 1) < levels.length ? 0x80000000 : 0), free+4+i*4
-    )
-
-  b.writeUInt32LE(free + 4 + levels.length*4, 0) //update free pointer.
-
-  return free
 }
 
 function r_levels (b, ptr) {
@@ -36,73 +15,70 @@ function r_levels (b, ptr) {
 }
 
 function compare (a, b) {
-  return a - b
+  return a < b ? -1 : a > b ? 1 : 0
 }
 
-function find(b, ptr, target) {
-  var level = r_levels(b, ptr)
-  var a = []
-  var ptr
-  var l= 100
-  while(l--) {
+function find(b, ptr, target, level) {
+  level = level || r_levels(b, ptr)
+
+  while(true) {
     if(level < 0) break;
     //if this level is last item
-    if(r_level(b, ptr, level) === 0) {
-      a.push(ptr)
+    if(r_level(b, ptr, level) === 0 || compare(r_value(b, r_level(b, ptr, level)), target) > 0)
       level --
-    }
-    //if current pointer is greater
-    else if (compare(r_value(b, r_level(b, ptr, level)), target) > 0) {
-      a.push(ptr)
+    else
+      ptr = r_level(b, ptr, level)
+  }
+
+  return ptr
+}
+
+
+function _insert (b, ptr, target, level) {
+  level = level || r_levels(b, ptr)
+  var free = b.readUInt32LE(0) || 8
+  //figure out before hand how many levels deep we want to insert this value
+  for(var _level = 0; _level < level && Math.random() > 0.5; )
+    _level ++
+
+  b.writeUInt32LE(free+4+4+_level*4, 0) //make space for this item.
+
+  //the value we want to insert
+  b.writeUInt32LE(target, free)
+  while(true) {
+    if(level < 0) break;
+    //if this level is last item
+    if(
+      r_level(b, ptr, level) === 0 ||
+      compare(r_value(b, r_level(b, ptr, level)), target) > 0
+    ) {
+      if(level <= _level)
+        b.writeUInt32LE(ptr, free+4+(4*level))
+
       level --
     }
     else {
       ptr = r_level(b, ptr, level)
     }
   }
-  return a
-}
 
-function _find(b, ptr, target, levels) {
-  var level = levels || 
-    r_levels(b, ptr)
-  var ptr
-  while(true) {
-    if(level < 0) break;
-    //if this level is last item
-    if(r_level(b, ptr, level) === 0)
-      level --
-    //if current pointer is greater
-    else if (compare(r_value(b, r_level(b, ptr, level)), target) > 0)
-      level --
-    else
-      ptr = r_level(b, ptr, level)
+  for(var i = 0; i <= _level; i++) {
+    var prev = b.readUInt32LE(free+4+i*4)
+    var next = b.readUInt32LE(prev+4+i*4) //includes msb continue flag
+    b.writeInt32LE(i < _level ? next | 0x80000000 : next & 0x7fffffff, free+4+i*4)
+    //free, free | (next & 0x80000000))
+    b.writeInt32LE(free | (next & 0x80000000), prev+4+i*4)
   }
-  return ptr
+
+  return free
 }
 
-
-var r = 0
-
-function insert (b, ptr, value) {
-  var levels = find(b, ptr, value)
-  var l = []
-  var free = b.readUInt32LE(0)
-  var again = ~~(Math.random()*(1<<30))
-  do {
-    var _ptr = levels.pop()
-    var val = b.readUInt32LE(_ptr)
-    var next = b.readUInt32LE(_ptr+4+4*l.length)
-    //write as int32 (not uint) to prevent out of bounds check
-    b.writeInt32LE(free | (next & 0x80000000), _ptr + 4 + 4*l.length)
-    l.push(next&0x7ffffff)
-    //again =>> 1
-//    var again = Math.random() > 0.5
-  } while(levels.length && ((again >>= 1) & 1)) //(again&1))
-  var c = item(b, value, l)
-
-  return c
+module.exports = {
+  item: item, dump: dump, find: find, all: all,
+  insert: _insert, get: r_value, next: r_level, levels: r_levels
 }
+
+//debugging stuff...
 
 function all (b) {
   var a = []
@@ -112,13 +88,6 @@ function all (b) {
   }
   return a
 }
-
-module.exports = {
-  item: item, dump: dump, find: _find, all: all,
-  insert: insert, get: r_value, next: r_level, levels: r_levels
-}
-
-//debugging stuff...
 
 function dump_node (b, ptr) {
   var levels = []
@@ -150,6 +119,26 @@ function dump (b) {
   }
   return a
 }
+
+function item (b, value, levels) {
+  if(levels.length < 1) throw new Error('levels must have at least [0]')
+  var free = r_value(b, 0) //read pointer to the very first item.
+  if(free == 0) {
+    free = 8
+    b.writeInt32LE(8, 4) //we are adding the first item, so set pointer.
+  }
+  b.writeUInt32LE(value, free)
+  for(var i = 0; i < levels.length; i++)
+    b.writeInt32LE(
+      levels[i] | ((i + 1) < levels.length ? 0x80000000 : 0), free+4+i*4
+    )
+
+  b.writeUInt32LE(free + 4 + levels.length*4, 0) //update free pointer.
+
+  return free
+}
+
+
 
 
 
